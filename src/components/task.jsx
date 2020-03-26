f391cb746750412e0c51a47ae91570654ff8c9a4
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-03-26 18:24:19 +0200

    add select state for tasks

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+  // task(key, name, selected)
+  constructor(props) {
+    super(props);
+  }
+
+  getClass(selected) {
+    return selected ? "task-selected" : "task"
+  }
+
+  handleClick() {
+    console.log('handleCLIckTASk');
+    console.log(this.props);
+    this.props.onClick(this.props.name, this.props.selected);
+  }
 
   render() { 
     return (  
-      <div className="task">
+      <div 
+        className={this.getClass(this.props.selected)}
+        onClick={this.handleClick.bind(this)}
+      >
         {this.props.name}
       </div>
     );
--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Task from './task'
 
 const tasks = [
-  { id: 1, name: "test task #1" },
-  { id: 2, name: "test task #2" },
-  { id: 3, name: "test task #3" },
-  { id: 4, name: "test task #4" },
-  { id: 5, name: "test task #5" }
+  { id: 1, name: "test task #1", selected: false },
+  { id: 2, name: "test task #2", selected: false },
+  { id: 3, name: "test task #3", selected: false },
+  { id: 4, name: "test task #4", selected: false },
+  { id: 5, name: "test task #5", selected: true }
 ]
 
 class Tasks extends Component {
@@ -14,21 +14,35 @@ class Tasks extends Component {
     super(props);
     this.state = {
       value: '',
-      items: []
+      items: tasks
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   
   handleSubmit(event) {
-    this.setState({ items: [ ...this.state.items, { "name": this.state.value, key: "should update" }] });
+    this.setState({ items: [ ...this.state.items, { "name": this.state.value, key: "should update", "selected": false }] });
     this.setState({ value: "" });
     event.preventDefault();
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
+  }
+
+  handleClick(name, isSelected) {
+    const itemIndex = this.state.items.findIndex( item => item.name == name );
+    const selected = !isSelected;
+    
+    this.setState({ items: this.state.items.map( item => item.name == name ? {...item, selected } : item) });
+  }
+
+  handleDelete() {
+    const newItems = this.state.items.filter( item => item.selected == true );
+    console.log(newItems);
   }
 
   render() { 
@@ -40,11 +54,10 @@ class Tasks extends Component {
         <input type="submit" value="Submit" />
       </form>
       
-      {tasks.map( el => <Task key={el.id} name={el.name}/> )}
-      {this.state.items.map( el => <Task key={el.id} name={el.name}/> )}
+      {this.state.items.map( el => <Task key={el.id} name={el.name} onClick={this.handleClick} selected={el.selected}/> )}
       
       <button className="btn">Update task</button>
-      <button className="btn">Delete task</button>
+      <button className="btn" onClick={this.handleDelete}>Delete task</button>
     </div>
     );
   }
